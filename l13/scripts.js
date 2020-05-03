083db1bf3231bc1ef7bebc500bcfa4c3f51b202b
Oleksii-Milkov <mouzer92@gmail.com>
2020-05-03 10:04:44 +0300

    Реализовал отправку письма на почту с помощью сервиса Mailtrap

--- l13/scripts.js
+++ l13/scripts.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
-const rl = require('readline-sync');
+const rl = require('readline-sync'); // npm install readline-sync
+const nm = require('nodemailer'); //npm install nodemailer
 
-console.log("1. Create\n2. Read\n3. Update\n4. Delete");
+console.log("1. Create\n2. Read\n3. Update\n4. Delete\n5. Extra");
 let answer = 'yes';
 
 while (answer == 'yes') {
@@ -38,7 +39,7 @@ while (answer == 'yes') {
             } else {
                 console.log("File is not found.");
             }
-            break;  
+            break;
 
         case '4':
             fileName = rl.question("Enter file name, what you want to delete: ");
@@ -50,9 +51,35 @@ while (answer == 'yes') {
             }
             break;
 
+        case '5':
+            let sender = nm.createTransport({
+                host: "smtp.mailtrap.io", //smtp сервер mailtrap
+                port: 2525,
+                auth: {
+                    user: "b20f9d06d7de61",
+                    pass: "12723c82b77f75"
+                }
+            });
+
+            let mailOptions = {
+                from: '2099812368-be35ec@inbox.mailtrap.io',
+                to: 'mouzer92@gmail.com',
+                subject: 'Учим nodemailer и SMTP',
+                text: 'SMTP — это простой протокол передачи почты. С английского языка переводится, как Simple Mail Transfer Protocol. Исходя из названия, можно сделать вывод, что SMTP сервер отвечает за отправку почтовых рассылок.'
+            };
+
+            sender.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+            break;
+
         default:
             console.log("This command does not exists.");
-            break;          
+            break;
     }
 
     answer = rl.question("Want to continue? (yes/no)");
