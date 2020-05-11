5fc7370c516ad88c9cf93e0261e405f745523b04
Oleksii-Milkov <mouzer92@gmail.com>
2020-05-11 16:40:04 +0300

    Третье задание, добавил .gitignore

--- l14/scripts.js
+++ l14/scripts.js
@@ -1,32 +1,37 @@
 //1-e задание ====================================================================
 
-const http = require('http');
+/* const http = require('http');
 const fs = require('fs');
 
 const port = 3000;
 
-http.createServer(function (request, response) {
-    if (request.url == '/') {
-        response.write('Hello, Node');
-        response.end();
-    } else if (request.url == '/about') {
-        console.log(request);
-        response.end();
-    } else if (request.url == '/contact') {
-        fs.readFile('index.html', function(err, data){
+http.createServer(function (req, res) {
+    if (req.url == '/') {
+        res.write('Hello, Node');
+        res.end();
+    } else if (req.url == '/about') {
+        console.log(req);
+        res.end();
+    } else if (req.url == '/contact') {
+        fs.readFile('index.html', function (err, data) {
             if (err) throw err;
-            response.end(data);
+            res.end(data);
         });
     } else {
-        response.write("Nothing to view");
-        response.end();
+        res.write("Nothing to view");
+        res.end();
     }
 }).listen(port, function () {
     console.log('Server at http://localhost:' + port);
-});
+}); */
 
 //3-e задание ====================================================================
 
+const request = require('request');
 
+request("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3", function (err, res, body) {
+    if (err) throw error;
+    console.log(JSON.parse(body));
+});
 
 //4-e задание ====================================================================
