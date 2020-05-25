a5337cb74b409146c44de2f9329107062797cf91
Oleksii-Milkov <mouzer92@gmail.com>
2020-05-25 17:19:10 +0300

    Код с 18-го занятия

---
+++ l18/rest_api/app.js
@@ -0,0 +1,54 @@
+const express = require('express'),
+    app = express();
+const port = 3000;
+
+app.use(express.json());
+
+let courses = [
+    {
+        id: 1,
+        name: "JS"
+    },
+    {
+        id: 2,
+        name: "Node JS"
+    },
+    {
+        id: 3,
+        name: "Angular"
+    }
+];
+
+app.get("/", function (req, res) {
+    res.send("Hello, world");
+});
+
+// Get all courses
+app.get("/courses", function (req, res) {
+    res.send(courses);
+});
+
+// Get course
+app.get("/api/courses/:id", function (req, res) {
+    let course = courses.find(function (course) {
+        return course.id === parseInt(req.params.id);
+    })
+    res.send(course);
+});
+
+//Post one course
+app.post("/api/courses", function (req, res) {
+    let course = {
+        id: Date.now(),
+        name: req.body.name
+    }
+
+    courses.push(course);
+    res.send(courses);
+});
+
+app.listen(port, function () {
+    console.log(`Express at http://localhost:${port}`);
+});
+
+
